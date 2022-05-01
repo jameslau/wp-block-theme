6af7cc7c33ca37d0e73b766bcff81c48e8b92423
James Lau <1402829+jameslau@users.noreply.github.com>
2022-04-30 21:17:07 -0400

    feat: (sec 4, chp 34) adding an option for toggling box shadow

--- wp-content/plugins/text-box/build/index.asset.php
+++ wp-content/plugins/text-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b58372374e7854a6253a07e725896f8f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd62ab17a46c0a43d1a82ee65c2026c57');
