13d3c331d25a8cd3223ddb77300504aca5dd1aa1
James Lau <1402829+jameslau@users.noreply.github.com>
2022-05-14 14:04:07 -0400

    feat: (sec 4, chp 38) adding deprecated versions for our block

--- wp-content/plugins/text-box/build/index.asset.php
+++ wp-content/plugins/text-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '13e3d24a11bb2fc52c6941e494af7a06');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9e51a1bab88be469641c5e47d63ab0c');
